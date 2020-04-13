c55e1bcff26dda9b0377990444482c492bf9bbd5
e-victoria <evictoria@protonmail.com>
2020-04-13 19:16:54 +0200

    Implement algorithm

--- binary_search/BinarySearch.js
+++ binary_search/BinarySearch.js
@@ -1,7 +1,22 @@
-export default class BinarySearch {
+class BinarySearch {
     getIsNumberInArray(array, targetNum) {
-        for (let el of array) {
-            console.log(el);
+        const sortedArray = array.sort((a, b) => a - b);
+        let middleElIndex = Math.floor((sortedArray.length - 1) / 2);
+
+        if (array.length === 1 && array[0] !== targetNum) {
+            return false;
+        }
+
+        if (sortedArray[middleElIndex] === targetNum) {
+            return true;
+        } else if (sortedArray[middleElIndex] > targetNum) {
+            const newArray = sortedArray.slice(0, middleElIndex);
+            return this.getIsNumberInArray(newArray, targetNum);
+        } else {
+            const newArray = sortedArray.slice(middleElIndex + 1, sortedArray.length);
+            return this.getIsNumberInArray(newArray, targetNum);
         }
     }
 }
+
+module.exports = BinarySearch;
