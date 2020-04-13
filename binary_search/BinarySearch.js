a683e99c8637c917973e4a4d33f37019b3b6dc9d
e-victoria <evictoria@protonmail.com>
2020-04-13 17:47:33 +0200

    Initial commit

---
+++ binary_search/BinarySearch.js
@@ -0,0 +1,7 @@
+export default class BinarySearch {
+    getIsNumberInArray(array, targetNum) {
+        for (let el of array) {
+            console.log(el);
+        }
+    }
+}
---
+++ binary_search/index.js
@@ -0,0 +1,4 @@
+import BinarySearch from './BinarySearch.js';
+
+const binarySearch = new BinarySearch();
+binarySearch.getIsNumberInArray([2, 43, 5], 2);
